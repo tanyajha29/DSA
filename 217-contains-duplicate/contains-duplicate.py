379700108667ac83f15169c2b39e36f17aaf0c20
Tanya jha <161499500+tanyajha29@users.noreply.github.com>
2026-05-20 23:35:29 +0530

    Time: 19 ms (59.97%) | Memory: 26 MB (10.22%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        seen = set()
+        for i in nums:
+            if i in seen:
+                return True
+            seen.add(i)
+        return False
+        
