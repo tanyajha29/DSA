ba6892c138f222d6ad13f277e0b07813d5bbe327
tanyajha29 <jhatanya211@gmail.com>
2026-05-29 00:08:35 +0530

    chore: stop tracking leetsync folders

--- 217-contains-duplicate/contains-duplicate.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def containsDuplicate(self, nums):
-        seen = set()
-        for i in nums:
-            if i in seen:
-                return True
-            seen.add(i)
-        return False
-        
--- 344-reverse-string/reverse-string.py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-    def reverseString(self, s):
-        left = 0
-        right = len(s) - 1
-        while left < right:
-            s[left], s[right] = s[right], s[left]
-            left += 1
-            right -= 1
