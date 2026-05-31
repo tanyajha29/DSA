574eb50dace54550334ad73ed6fb3b4360e97216
Tanya jha <161499500+tanyajha29@users.noreply.github.com>
2026-05-31 18:17:26 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (92.87%) - LeetSync

---
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.py
@@ -0,0 +1,41 @@
+class MyQueue(object):
+
+    def __init__(self):
+        # initialize 2 array for storing and reversing the stack
+        # 1. stack 1 which is for orginal elements storing
+        self.instack = []
+
+        #2. stack 2 will store the reversed elements from stack1
+        self.outstack = []
+
+    def push(self, x):
+        #stor the element is stack 1
+       self.instack.append(x)
+        
+
+    def pop(self):
+        # check if there are elements in stack 2 or not
+        self.peek()
+
+        return self.outstack.pop()
+        
+
+    def peek(self):
+        # store the elements in stack2 only if necessary 
+        if not self.outstack :
+            while self.instack :
+                self.outstack.append(self.instack.pop())
+        return self.outstack[-1]
+        
+
+    def empty(self):
+        return ( len(self.instack) == 0 and len(self.outstack) == 0)
+        
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
